87ab05bcbd428ba104ba988d3796e5f445003f1f
Shadmage <admin@dirtydoggaming.co.za>
2023-03-23 16:57:18 +0200

    Added todo list

---
+++ src/main/java/dev/shadmage/bungeetest/ToDo.java
@@ -0,0 +1,32 @@
+package dev.shadmage.bungeetest;
+
+public class ToDo {
+
+	/*
+
+	 * Plugin messaging
+
+	 * Luckperms integration
+	 	* Listen for changes to player groups... syncronise with other servers on the network and to a central DB (luckperms bungee DB?)
+
+	* Other potential improvements?
+
+	* New player join broadcast messages
+
+	* playtime rewards
+
+	* voting rewards?
+
+	* perhaps this could replace the actions web shop performs,
+		enable /redeem type command system on the server
+		this one plugin ensures players receive the items they have purchased, and the shop can update a DB this plugin pulls from
+		on a timer
+			-get all online players
+			-check shop db for outstanding purchases
+			-give player item
+			-update shop DB, as given
+			- log to console and a seperate log file that item/perk has been given (log date, time, item given, player name, uuid, and IP
+
+
+	 */
+}
